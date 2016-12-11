587db126ca2e07946204e70153bc17228d01fb7c
Justin P <justpunz@live.com>
2016-12-10 23:08:30 -0800

    Fixed some problems

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var bodyParser = require('body-parser');
 var cleverbot = require("cleverbot.io"),
 bot = new cleverbot("CQr857BJmzen8Nrw", "3psJeIWm9nLZaupKUAfvTb886EFln5sb");
 
+bot.setNick("sessionname");
+
+bot.create(function (err, session) {
+
+  // Woo, you initialized cleverbot.io.  Insert further code here
+});
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,13 +20,6 @@ app.post('/message', function(req, res){
 
 	var msgBody = req.body.Body;
 	var cleverResponse = "";
-
-	bot.setNick("sessionname");
-
-	bot.create(function (err, session) {
-
-	  // Woo, you initialized cleverbot.io.  Insert further code here
-	});
 
 	bot.ask(msgBody, function (err, response) {
 	cleverResponse = response;
